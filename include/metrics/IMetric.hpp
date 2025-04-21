18f1fd058e6e693337855c08c00dd722099757e7
Anton Panyushev <FFG99P@yandex.rru>
2025-04-21 19:10:09 +0300

    Create interface for metrics

---
+++ include/metrics/IMetric.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <map>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <variant>
+#include <vector>
+
+using json = nlohmann::json;
+using MetricValue =
+    std::variant<int, double, std::vector<int>, std::vector<double>,
+                 std::map<std::string, double>>;
+
+class IMetric {
+public:
+    virtual ~IMetric() = default;
+
+    virtual MetricValue collect() const = 0;
+
+    virtual bool is_valid() const = 0;
+
+    virtual std::string name() const = 0;
+};
